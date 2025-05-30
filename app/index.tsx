a35f27fde06bc3f5615b10ae281f652b49cc15a0
dindiongco <davidindiongco1@gmail.com>
2025-05-30 19:55:04 +0100

    Initial setup

---
+++ app/index.tsx
@@ -0,0 +1,29 @@
+ import { Text, View, StyleSheet } from 'react-native';
+ import { Link } from 'expo-router'; 
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+      <Link href="/profile" style={styles.button}>
+        Go to About screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#25292e',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#25292e',
+  },
+});
